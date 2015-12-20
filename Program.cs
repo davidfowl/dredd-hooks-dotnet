46fb6eaed70bf6a63f279558a0d8f3722f3a9aff
XVincentX <vincenz.chianese@yahoo.it>
2015-12-20 22:46:25 +0100

    Basic boilerplate

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace dredd_hooks_dotnet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+			Console.WriteLine("Hello from DNX!");
+        }
+    }
+}
