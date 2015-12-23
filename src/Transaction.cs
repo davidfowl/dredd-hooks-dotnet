1643c49dbd26a4521374b5d2b8070bce11da1f39
XVincentX <vincenz.chianese@yahoo.it>
2015-12-23 09:58:53 +0100

    Transaction event type is not a string

--- src/Transaction.cs
+++ src/Transaction.cs
@@ -3,7 +3,7 @@ namespace dredd_hooks_dotnet
   public struct Transaction 
   {
     public string uuid { get; set; }
-    public EventType Event { get; set; }
+    public string Event { get; set; }
     public object Data { get; set; }
   }
 }
