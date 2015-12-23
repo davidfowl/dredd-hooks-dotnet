ee498b6ec45250c76ecd901554c7912f3b16417c
XVincentX <vincenz.chianese@yahoo.it>
2015-12-23 09:58:05 +0100

    Transformed enum to static strings (enum are integers!

--- src/EventType.cs
+++ src/EventType.cs
@@ -1,14 +1,14 @@
 namespace dredd_hooks_dotnet
 {
-  public enum EventType 
+  public static class EventType 
   {
-    beforeAll = 0,
-    beforeEach,
-    before,
-    beforeEachValidation,
-    beforeValidation,
-    after,
-    afterEach,
-    afterAll,
+    public static string beforeAll = "beforeAll";
+    public static string beforeEach = "beforeEach";
+    public static string before = "before";
+    public static string beforeEachValidation = "beforeEachValidation";
+    public static string beforeValidation = "beforeValidation";
+    public static string after = "after";
+    public static string afterEach = "afterEach";
+    public static string afterAll = "afterAll";
   }
 }
