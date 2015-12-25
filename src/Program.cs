03ede0a2e41f96534cf2dc5bfd25bed87a5e7b85
XVincentX <vincenz.chianese@yahoo.it>
2015-12-25 09:57:04 +0100

    Added checks for file existence

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,24 @@
+using System;
+using System.IO;
+
 namespace dredd_hooks_dotnet
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+          if (args.Length == 0)
+          {
+            Console.WriteLine("Hooks file name not specified.");
+            return;
+          }
+          
+          if (!File.Exists(args[0]))
+          {
+            Console.WriteLine("Specified hook file does not exist");
+            return;
+          }
+          
           Server s = new Server();
           s.Run().Wait();
         }
