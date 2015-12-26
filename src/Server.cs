7f9a00178fc180e9a9547381e203307d3ae6a517
XVincentX <vincenz.chianese@yahoo.it>
2015-12-26 16:40:30 +0100

    Do not use offset, it will advance the stream by itself

--- src/Server.cs
+++ src/Server.cs
@@ -41,33 +41,46 @@ namespace dredd_hooks_dotnet
         listener.Start();
 
         var stringBuffer = new StringBuilder(100);
+        while (true)
+        {
+          var clientConnection = await listener.AcceptTcpClientAsync();
 
-        var clientConnection = await listener.AcceptTcpClientAsync();
-        var dataStream = clientConnection.GetStream();
+          var dataStream = clientConnection.GetStream();
+          
+          int readBytes = 0;
 
-        int offset = 0;
-        int readBytes = 0;
+          do {
+            readBytes = await dataStream.ReadAsync(buffer, 0, readSize);
+            var messagePart = MessageEncoding.GetString(buffer,0, readBytes);
 
-        do {
-          readBytes = await dataStream.ReadAsync(buffer, offset, readSize);
-          var messagePart = MessageEncoding.GetString(buffer,0, readBytes);
-
-          if (messagePart.Contains(Environment.NewLine))
-          {
-            var split = messagePart.Split(messageSeparator);
-            var message = string.Concat(stringBuffer.ToString(), split[0]);
-            stringBuffer.Clear().Append(split[1]);
-            var response = ProcessMessage(JsonConvert.DeserializeObject<HookTransaction>(message));
-            var responseBytes = MessageEncoding.GetBytes(string.Concat(JsonConvert.SerializeObject(response),messageSeparator));
-            await dataStream.WriteAsync(responseBytes, 0, responseBytes.Length);
-          }
-          else
-          {
-            stringBuffer.Append(messagePart);
-          }
-
-          offset += readBytes;
-        } while (readBytes > 0);
+            if (messagePart.Contains(Environment.NewLine))
+            {
+              var split = messagePart.Split(messageSeparator);
+              var message = string.Concat(stringBuffer.ToString(), split[0]);
+              stringBuffer.Clear().Append(split[1]);
+              try 
+              {
+                Console.Out.WriteLine("Ok I have a full message");
+                Console.Out.WriteLine(message);
+                var deserializedMessage = JsonConvert.DeserializeObject<HookTransaction>(message);
+                var response = ProcessMessage(deserializedMessage);
+                var serializedResponse = JsonConvert.SerializeObject(response);
+                var responseBytes = MessageEncoding.GetBytes(string.Concat(serializedResponse, messageSeparator));
+                await dataStream.WriteAsync(responseBytes, 0, responseBytes.Length);                
+              }
+              catch (JsonSerializationException) 
+              {
+                Console.Out.WriteLine("Invalid message. Skipped.");
+                continue;
+              }
+            }
+            else
+            {
+              stringBuffer.Append(messagePart);
+            }
+          } while (readBytes > 0);
+          
+        }
       }
     }
 }
